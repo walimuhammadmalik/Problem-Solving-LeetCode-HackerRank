4ce2a4cdfd9ea5b65ebc6fa25206f440117d2dc6
Wali Muhammad <136965794+thewalimuhammad@users.noreply.github.com>
2024-08-06 23:03:03 +0500

    Create leetcode.com problems remove-element.js

---
+++ leetcode.com problems remove-element.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+var removeElement = function(nums, val) {
+    let k = 0; // Pointer for the position of the next non-val element
+
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] !== val) {
+            nums[k] = nums[i];
+            k++;
+        }
+    }
+
+    return k;
+};
