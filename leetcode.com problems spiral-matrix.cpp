58cb8e8f41e23c0f121aacf63828e9c7bf6d6ccc
Wali Muhammad <136965794+thewalimuhammad@users.noreply.github.com>
2024-11-23 23:07:16 +0500

    Create leetcode.com problems spiral-matrix.cpp

---
+++ leetcode.com problems spiral-matrix.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+        vector<int> result;
+        if (matrix.empty() || matrix[0].empty()) {
+            return result; // Edge case: empty matrix
+        }
+
+        int top = 0, bottom = matrix.size() - 1;
+        int left = 0, right = matrix[0].size() - 1;
+
+        while (top <= bottom && left <= right) {
+            // Traverse from left to right on the top row
+            for (int i = left; i <= right; ++i) {
+                result.push_back(matrix[top][i]);
+            }
+            top++;
+
+            // Traverse from top to bottom on the right column
+            for (int i = top; i <= bottom; ++i) {
+                result.push_back(matrix[i][right]);
+            }
+            right--;
+
+            if (top <= bottom) {
+                // Traverse from right to left on the bottom row
+                for (int i = right; i >= left; --i) {
+                    result.push_back(matrix[bottom][i]);
+                }
+                bottom--;
+            }
+
+            if (left <= right) {
+                // Traverse from bottom to top on the left column
+                for (int i = bottom; i >= top; --i) {
+                    result.push_back(matrix[i][left]);
+                }
+                left++;
+            }
+        }
+
+        return result;
+    }
+};
